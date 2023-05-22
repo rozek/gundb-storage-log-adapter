329cc39b399edcb72d2a168b8371a1fe7cbe2c44
Andreas Rozek <A.Rozek@gmx.de>
2023-05-22 17:57:24 +0200

    Create StorageLogAdapter.js

---
+++ src/StorageLogAdapter.js
@@ -0,0 +1,24 @@
+/**** storage log adapter ****/
+
+  GUN.on('create', function (Context) {        // do not use a "fat arrow" here!
+    this.to.next(Context)
+
+    if (Context.opt.flowlog != true) { return }      // adapter wasn't requested
+
+  /**** get - retrieve the contents of a given node ****/
+
+    Context.on('get', function (WireMessage) {           // no "fat arrow" here!
+      this.to.next(WireMessage)
+      console.log('! GET',WireMessage.get)
+      GUN.on.get.ack(WireMessage,undefined)
+    })
+
+  /**** put - patches the contents of a given node ****/
+
+    Context.on('put', function (WireMessage) {           // no "fat arrow" here!
+      this.to.next(WireMessage)
+      console.log('! PUT',WireMessage.put)
+      Context.on('in', { '@':WireMessage['#'], err:undefined, ok:1 })
+    })
+  })
+
